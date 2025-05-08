c65c423c62f378d8f148f3affa02f5ec5e0a5678
John Lee <John@ehicas.com.vn>
2025-05-08 16:41:10 +0700

    update

--- output/1746696763995_test.js
+++
@@ -1,8 +0,0 @@
-import testControllerHolder from "E:/Synergis/TestCafe/CodeceptJS/node_modules/codeceptjs/lib/helper/testcafe/testControllerHolder.js";
-
-
-    fixture("fixture")
-
-    test
-
-    ("test", testControllerHolder.capture)
